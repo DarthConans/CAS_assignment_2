8ad3b7b022c7e1de297c3d942ed3bb1b7e61d6ca
Merek Grimaldus <mordiggian98@gmail.com>
2023-04-02 15:09:53 -0600

    updated titer_table.csv

--- mutation_map_generator.py
+++ mutation_map_generator.py
@@ -5,6 +5,7 @@ import matplotlib.colors as cls
 import matplotlib.patches as patches
 from utils import *
 
+
 # create a graph where each node represents
 # a unique genetic sequence, and edges
 # represent a single mutation difference
@@ -27,7 +28,7 @@ def create_graph(sequences, fitness_change, antigen_difference, original_length)
         saturation_f = ((fitness_change[i] - f_min) / f_range) * (0.8) + 0.2
         saturation_a = ((antigen_difference[i] - a_min) / a_range) * (0.8) + 0.2
         value = 0.8
-        if antigen_difference[i]==0:
+        if antigen_difference[i] == 0:
             value = 0
         color_map_fitness.append(cls.to_hex(cls.hsv_to_rgb([0.37, saturation_f, value])))
         color_map_antigen.append(cls.to_hex(cls.hsv_to_rgb([0.0, saturation_a, value])))
@@ -37,9 +38,9 @@ def create_graph(sequences, fitness_change, antigen_difference, original_length)
     # add edges
     for i in range(len(sequences)):
         for j in range(i, len(sequences)):
-            if i!=j and one_mutation_away(sequences[i], sequences[j]):
+            if i != j and one_mutation_away(sequences[i], sequences[j]):
                 G.add_edge(i, j)
-    
+
     fig = plt.figure(dpi=240)
     fig.add_subplot(1, 2, 1)
     nx.draw(G, pos=nx.spring_layout(G, seed=31), node_size=10, node_color=color_map_fitness, edge_color="gray")
@@ -58,14 +59,15 @@ def create_graph(sequences, fitness_change, antigen_difference, original_length)
 
     plt.savefig("Mutation_Network.png")
 
+
 def one_mutation_away(seq1, seq2):
     mutations = 0
     for i in range(len(seq1)):
         if seq1[i] != seq2[i]:
-            mutations+=1
+            mutations += 1
         if mutations > 1:
             return False
-    return mutations==1    
+    return mutations == 1
 
 
 def get_intermediate_mutations(original, sequences):
@@ -73,7 +75,7 @@ def get_intermediate_mutations(original, sequences):
     for s in sequences:
         for i, n in enumerate(s):
             if original[i] != n:
-                inter = s[:i]+original[i]+s[i+1:]
+                inter = s[:i] + original[i] + s[i + 1:]
                 if inter not in intermediate:
                     intermediate.append(inter)
     return intermediate
@@ -87,13 +89,14 @@ bind_BQ11 = BindingCalculator(known_to_neutralize="BQ.1.1", weight_by_log_IC50=F
 bind_D614G = BindingCalculator(known_to_neutralize="D614G", weight_by_log_IC50=False)
 bind_XBB = BindingCalculator(known_to_neutralize="XBB", weight_by_log_IC50=False)
 
+
 def get_titer_table(sites):
     # testing against antibodies known to neutralize BA.1 BA.2 BA.275 BQ.1.1 D614G XBB
     columns = ["", "BA.1", "BA.2", "BA.2.75", "BA.5", "BQ.1.1", "D614G", "XBB"]
     titer = []
     for i in range(len(sites)):
         titer.append([
-            "Seq"+str(i),
+            "Seq" + str(i),
             bind_BA1.binding_retained({sites[i]}),
             bind_BA2.binding_retained({sites[i]}),
             bind_BA275.binding_retained({sites[i]}),
@@ -101,26 +104,47 @@ def get_titer_table(sites):
             bind_BQ11.binding_retained({sites[i]}),
             bind_D614G.binding_retained({sites[i]}),
             bind_XBB.binding_retained({sites[i]})
-            ])
+        ])
+    for name, site_list in [
+        ("alpha", [501]),
+        ("beta", [417, 484, 501]),
+        ("gamma", [417, 484, 501]),
+        ("delta", [452, 478]),
+        ("lambda", [452, 490]),
+        ("mu", [346, 484, 501]),
+        ("omicron", [339, 371, 373, 375, 417, 440, 446, 477, 478, 484, 493, 496, 498, 501, 505]),
+        ("pms_20", [346, 417, 440, 445, 455, 475, 484, 501])
+    ]:
+        titer.append([
+            name,
+            bind_BA1.binding_retained(site_list),
+            bind_BA2.binding_retained(site_list),
+            bind_BA275.binding_retained(site_list),
+            bind_BA5.binding_retained(site_list),
+            bind_BQ11.binding_retained(site_list),
+            bind_D614G.binding_retained(site_list),
+            bind_XBB.binding_retained(site_list)
+
+        ])
     frame = pd.DataFrame(columns=columns, data=titer)
     frame.to_csv("results/titer_table.csv", index=False)
-    #titer.to_csv("results/titer_table.csv", index=False)
+    # titer.to_csv("results/titer_table.csv", index=False)
 
 
 if __name__ == "__main__":
     data = pd.read_csv("results/fitness_change_of_antigenically_non_neutral_2_hop.csv", index_col=False)
     # Get the top value in the old sequence column
     old_sequence = data["old sequence"].iloc[0]
-    #Down select
+    # Down select
     data = data[["site", "fitness change", "antigen difference", "new sequence"]]
-    #Select where anitgen difference above average
+    # Select where anitgen difference above average
     mean = data["antigen difference"].mean()
-    #Select only positive fitness change
+    # Select only positive fitness change
     data = data[(data["fitness change"] > 0)]
-    #Code for above average antigen difference, turned off right now
-    #data = data[(data["antigen difference"] > mean)]
+    # Code for above average antigen difference, turned off right now
+    # data = data[(data["antigen difference"] > mean)]
     data.to_csv("results/down_selected.csv", index=False)
-    #List of sequences
+    # List of sequences
     sequences = list(data["new sequence"])
 
     # get titer table values
@@ -139,10 +163,10 @@ if __name__ == "__main__":
     # add intermediate mutations between
     # original sequences and new sequences
     intermediate_mutations = get_intermediate_mutations(old_sequence, sequences)
-    sequences = sequences+intermediate_mutations
+    sequences = sequences + intermediate_mutations
     fitness_change = fitness_change + ([0] * len(intermediate_mutations))
     antigen_difference = antigen_difference + ([0] * len(intermediate_mutations))
 
-    #create_graph(sequences, fitness_change, antigen_difference, original_length)
+    # create_graph(sequences, fitness_change, antigen_difference, original_length)
 
     print("krewl")
